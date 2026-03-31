84d87f5292c6c182b13b5530444f469e2b8fef5d
p12duke <pt12thduke@gmail.com>
2026-04-01 00:12:40 +0530

    Added Custom completions for -v and -n switches in the script

---
+++ scr_j.bash
@@ -0,0 +1,14 @@
+source ~/bash_bin/bash_gen/script_journal.sh
+
+_scj_completions()
+{
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+
+    # Example suggestions (you can customize this list)
+		local options="$(ls ~/script_logs/)"
+				COMPREPLY+=( $(compgen -W "${options}" -- "$cur") )
+}
+
+complete -F _scj_completions sr -n  sr -v
+
+
